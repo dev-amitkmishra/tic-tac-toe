eff51af14dad9577fcf3751bb0b477f75b80ddef
amit.mishra <amit.mishra@soprasteria.com>
2020-03-25 19:34:42 +0530

    board added

---
+++ main.py
@@ -0,0 +1,13 @@
+#create board with index
+from IPython.display import clear_output
+
+def display_board(board):
+    clear_output()
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('-----')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-----')
+    print(board[1] + '|' + board[2] + '|' + board[3])
+
+#print the output
+display_board(['#','X','O','X','O','X','O','X','O','X'])
