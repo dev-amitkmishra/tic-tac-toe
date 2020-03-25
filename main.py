c041506ac5f78e398f8754e09605c1fa41d10d4c
amit.mishra <amit.mishra@soprasteria.com>
2020-03-25 20:23:27 +0530

    board check and conditional check for user input

--- main.py
+++ main.py
@@ -31,3 +31,42 @@ def player_input():
 
 #tuple unpacking to get the marker of both player
 player1_marker, player2_marker = player_input()
+
+#place marker
+def place_marker(board, marker, position):
+    board[position] = marker
+
+# check whether new data is updating or not in board
+test_board = ['#','X','O','X','O','X','O','X','O','X']
+place_marker(test_board, '%', 5)
+display_board(test_board)
+
+#win check of any player
+def win_check(board, mark):
+    #won the game or not
+    return ((board[1] == board[2] == board[3] == mark) or 
+            (board[4] == board[5] == board[6] == mark) or 
+            (board[7] == board[8] == board[9] == mark) or 
+            (board[1] == board[4] == board[7] == mark) or 
+            (board[2] == board[5] == board[8] == mark) or 
+            (board[3] == board[6] == board[9] == mark) or 
+            (board[1] == board[5] == board[9] == mark) or 
+            (board[3] == board[5] == board[7] == mark))
+
+# check who will start first
+import random
+
+def choose_first():
+    flip = random.randint(0, 1)
+    return 'Player 1' if flip == 0 else 'Player 2'
+
+# check position on board is empty or not
+def space_check(board, position):
+    return board[position] == ''
+
+# check full board is empty or not
+def full_board_check(board):
+    for i in range(1, 10):
+        if space_check(board, i):
+            return False
+    return True
