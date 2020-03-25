5141a0db75d5d9c80f6f5e3c2dd7706ea53b8389
amit.mishra <amit.mishra@soprasteria.com>
2020-03-25 20:29:42 +0530

    check player choice on board and replay

--- main.py
+++ main.py
@@ -70,3 +70,14 @@ def full_board_check(board):
         if space_check(board, i):
             return False
     return True
+
+# check position provided by user exists, empty or not on board
+def player_choice(board):
+    position = 0
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose next position from [1,9]'))
+    return position
+
+# check player want to play again
+def replay():
+    return input('Do you want to play again? Enter Yes or No: ').lower().startswith('y')
